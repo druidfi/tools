5176699926fc8b756647330af35d14443ca23806
Marko Korhonen <marko.korhonen@druid.fi>
2019-02-15 10:58:45 +0200

    phpunit

--- make/composer.mk
+++ make/composer.mk
@@ -1,7 +1,9 @@
 BUILD_TARGETS := vendor
 CLEAN_FOLDERS += vendor
 COMPOSER_VENDOR_BIN := vendor/bin
-PHPUNIT_BIN := $(shell test -f vendor/bin/phpunit && echo vendor/bin/phpunit || echo no)
+PHPUNIT_BIN := ${COMPOSER_VENDOR_BIN}/phpunit
+PHPUNIT_BIN_EXISTS := $(shell test -f ${PHPUNIT_BIN} && echo yes || echo no)
+TEST_TARGETS += test-phpunit
 
 PHONY += composer-update
 composer-update: ## Update Composer packages
@@ -16,12 +18,13 @@ vendor: composer.json composer.lock ## Install Composer packages
 	$(call colorecho, "\nDo Composer install (${RUN_ON})...\n")
 	$(call composer_on_${RUN_ON},install)
 
-ifneq ($(PHPUNIT_BIN),no)
-	TEST_TARGETS += test-phpunit
-
-	PHONY += test-phpunit
-	test-phpunit: ## Run PHPUnit tests
-		$(call call_in_root,${PHPUNIT_BIN} -c phpunit.xml.dist --testsuite unit)
+PHONY += test-phpunit
+test-phpunit: ## Run PHPUnit tests
+	@echo "- ${YELLOW}test-phpunit:${NO_COLOR} Start running PHPUnit tests..."
+ifeq (${PHPUNIT_BIN_EXISTS},yes)
+	$(call call_in_root,${PHPUNIT_BIN} -c phpunit.xml.dist --testsuite unit)
+else
+	@echo "- ${YELLOW}${PHPUNIT_BIN} does not exist! ${RED}[ERROR]${NO_COLOR}"
 endif
 
 define composer_on_docker
