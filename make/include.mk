36b250df0e3c142a7530123845d0f7282bb0a69a
Marko Korhonen <marko.korhonen@druid.fi>
2019-02-15 08:09:57 +0200

    Include composer.mk if composer.json exists

--- make/include.mk
+++ make/include.mk
@@ -1,5 +1,7 @@
 include $(DRUIDFI_TOOLS_MAKE_DIR)common.mk
 include $(DRUIDFI_TOOLS_MAKE_DIR)docker.mk
-include $(DRUIDFI_TOOLS_MAKE_DIR)composer.mk
+ifeq ($(COMPOSER_JSON_EXISTS),yes)
+	include $(DRUIDFI_TOOLS_MAKE_DIR)composer.mk
+endif
 include $(DRUIDFI_TOOLS_MAKE_DIR)drupal.mk
 include $(DRUIDFI_TOOLS_MAKE_DIR)amazeeio.mk
