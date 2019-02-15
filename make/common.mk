6a58e3d3995e09e666b35ba0f1d52f0800bc35d9
Marko Korhonen <marko.korhonen@druid.fi>
2019-02-15 07:30:50 +0200

    updated self-update

--- make/common.mk
+++ make/common.mk
@@ -21,6 +21,6 @@ help: ## List all make commands
 	@cat $(MAKEFILE_LIST) | grep -e "^[a-zA-Z_\-]*: *.*## *" | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' | sort
 
 PHONY += self-update
-self-update: ## Self-update all the tools from druidfi/tools
-	$(call colorecho, "BETA: self update with Composer")
-	$(call composer_on_${RUN_ON},update druidfi/tools --no-plugins)
+self-update: ## Self-update makefiles from druidfi/tools
+	$(call colorecho, "Update makefiles from druidfi/tools")
+	@sh -c "$(curl -fsSL https://raw.githubusercontent.com/druidfi/tools/master/update.sh)"
