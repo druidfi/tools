036d33149b6db610c76aa8ee8184cb4481707bd6
Marko Korhonen <marko.korhonen@druid.fi>
2019-02-15 11:14:54 +0200

    still use Composer scripts to run bins loaded with Composer

--- make/composer.mk
+++ make/composer.mk
@@ -3,7 +3,9 @@ CLEAN_FOLDERS += vendor
 COMPOSER_VENDOR_BIN := vendor/bin
 PHPUNIT_BIN := ${COMPOSER_VENDOR_BIN}/phpunit
 PHPUNIT_BIN_EXISTS := $(shell test -f ${PHPUNIT_BIN} && echo yes || echo no)
-TEST_TARGETS += test-phpunit
+TEST_TARGETS += fix test-phpunit
+PHPCBF_BIN := ${COMPOSER_VENDOR_BIN}/phpcbf
+PHPCBF_BIN_EXISTS := $(shell test -f ${PHPCBF_BIN} && echo yes || echo no)
 
 PHONY += composer-update
 composer-update: ## Update Composer packages
@@ -12,7 +14,12 @@ composer-update: ## Update Composer packages
 
 PHONY += fix
 fix: ## Fix code style
-	$(call ${RUN_ON},fix)
+	@echo "- ${YELLOW}fix:${NO_COLOR} Start PHP Code Beautifier and Fixer..."
+ifeq (${PHPCBF_BIN_EXISTS},yes)
+	$(call composer_on_${RUN_ON},fix)
+else
+	@echo "- ${YELLOW}${PHPCBF_BIN} does not exist! ${RED}[ERROR]${NO_COLOR}"
+endif
 
 vendor: composer.json composer.lock ## Install Composer packages
 	$(call colorecho, "\nDo Composer install (${RUN_ON})...\n")
@@ -22,7 +29,7 @@ PHONY += test-phpunit
 test-phpunit: ## Run PHPUnit tests
 	@echo "- ${YELLOW}test-phpunit:${NO_COLOR} Start running PHPUnit tests..."
 ifeq (${PHPUNIT_BIN_EXISTS},yes)
-	$(call call_in_root,${PHPUNIT_BIN} -c phpunit.xml.dist --testsuite unit)
+	$(call composer_on_${RUN_ON},test-unit)
 else
 	@echo "- ${YELLOW}${PHPUNIT_BIN} does not exist! ${RED}[ERROR]${NO_COLOR}"
 endif
