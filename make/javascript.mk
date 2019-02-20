40f651dd9094f087f3249ef8f9fda4e360542b6f
Marko Korhonen <marko.korhonen@druid.fi>
2019-02-20 06:42:57 +0200

    javascript.mk

---
+++ make/javascript.mk
@@ -0,0 +1,13 @@
+BUILD_TARGETS := node_modules
+
+node_modules: package.json
+	$(call colorecho, "\n-So npm install (${RUN_ON})...\n")
+	$(call npm_on_${RUN_ON},install --engine-strict true)
+
+define npm_on_docker
+	$(call call_in_root,npm $(1))
+endef
+
+define npm_on_host
+	@npm $(1)
+endef
