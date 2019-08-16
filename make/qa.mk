63293276975732c72a346cc19547b6867967e41e
Marko Korhonen <marko.korhonen@druid.fi>
2019-08-16 13:58:16 +0300

    Quick fix on make test

--- make/qa.mk
+++ make/qa.mk
@@ -1,6 +1,6 @@
 TEST_TARGETS += lint
-LINT_PATHS_JS += ./$(DRUPAL_WEBROOT)/modules/custom/*/js
-LINT_PATHS_JS += ./$(DRUPAL_WEBROOT)/themes/custom/*/js
+LINT_PATHS_JS += ./$(WEBROOT)/modules/custom/*/js
+LINT_PATHS_JS += ./$(WEBROOT)/themes/custom/*/js
 
 PHONY += lint
 lint: lint-php lint-js ## Check code style
@@ -10,11 +10,11 @@ lint-js: DOCKER_NODE_IMG ?= node:8.16.0-alpine
 lint-js: WD := /usr/src/app
 lint-js: ## Check code style for JS files
 	$(call step,Install linters...)
-	@docker run --rm -v "$(CURDIR)":$(WD) -w $(WD) $(DOCKER_NODE_IMG) yarn --cwd $(DRUPAL_WEBROOT)/core install
+	@docker run --rm -v "$(CURDIR)":$(WD) -w $(WD) $(DOCKER_NODE_IMG) yarn --cwd $(WEBROOT)/core install
 	$(call step,Check code style for JS files: $(DRUPAL_LINT_PATHS))
 	@docker run --rm -v "$(CURDIR)":$(WD) -w $(WD) $(DOCKER_NODE_IMG) \
-		$(DRUPAL_WEBROOT)/core/node_modules/eslint/bin/eslint.js --color --ignore-pattern '**/vendor/*' \
-		--c ./$(DRUPAL_WEBROOT)/core/.eslintrc.json --global nav,moment,responsiveNav:true $(LINT_PATHS_JS)
+		$(WEBROOT)/core/node_modules/eslint/bin/eslint.js --color --ignore-pattern '**/vendor/*' \
+		--c ./$(WEBROOT)/core/.eslintrc.json --global nav,moment,responsiveNav:true $(LINT_PATHS_JS)
 
 PHONY += lint-php
 lint-php: ## Check code style for PHP files
@@ -23,7 +23,7 @@ lint-php: ## Check code style for PHP files
 
 PHONY += test
 test: ## Run tests
-	$(call step,Run tests:\n- Following test targets will be run: $(TEST_TARGETS)")
+	$(call step,Run tests:\n- Following test targets will be run: $(TEST_TARGETS))
 	@$(MAKE) $(TEST_TARGETS)
 	$(call step,Tests completed.)
 
