06b8a8d4e7462bd63bcfbf166a44fd9b65a16490
Marko Korhonen <marko.korhonen@druid.fi>
2019-10-09 09:19:54 +0300

    Cleanup on tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,15 +5,9 @@ TMP_FILE=/tmp/druidfi-tools-comparison.txt
 
 for item in ${TEST_FILES[*]}
 do
-    #printf "%s\n" "$item"
-    #COMPARE=$(cat "${item}")
     MAKE_TARGET=$(head -n1 "${item}")
     EXPECTED=$(tail -n +3 "${item}")
     OUTPUT=$(make -n --no-print-directory --directory=make ${MAKE_TARGET})
-    #printf "%s" "${COMPARE}\n"
-    #printf "\n\n1: %s\n" "${MAKE_TARGET}"
-    #printf "2: %s\n" "${EXPECTED}"
-    #printf "2: %s\n" "${OUTPUT}"
 
     if [ "${OUTPUT}" = "${EXPECTED}" ]
     then
