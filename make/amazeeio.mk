6bb642599ae487f544e55d59b50d418cc2e5b968
Marko Korhonen <marko.korhonen@druid.fi>
2019-11-21 07:08:55 +0200

    Allow amazee setup to be overriden with .env

--- make/amazeeio.mk
+++ make/amazeeio.mk
@@ -1,12 +1,12 @@
 ifeq ($(AMAZEEIO_LAGOON),yes)
-	CLI_SERVICE := cli
+	CLI_SERVICE ?= cli
 else
-	CLI_SERVICE := drupal
-	CLI_USER := drupal
-	DOCKER_PROJECT_ROOT := /var/www/drupal/public_html
+	CLI_SERVICE ?= drupal
+	CLI_USER ?= drupal
+	DOCKER_PROJECT_ROOT ?= /var/www/drupal/public_html
 endif
 
-CLI_SHELL := bash
+CLI_SHELL ?= bash
 
 PHONY += amazeeio-env
 amazeeio-env: ## Print Amazee.io env variables
