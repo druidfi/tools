d252047297c5a7570f31ac2af5499e93cedc9446
Marko Korhonen <marko.korhonen@druid.fi>
2020-06-15 09:11:23 +0300

    Make DOCKER_COMPOSE_EXEC var overridable

--- make/docker.mk
+++ make/docker.mk
@@ -2,7 +2,7 @@ CLI_SERVICE := cli
 CLI_SHELL := sh
 CLI_USER := root
 DOCKER_COMPOSE := docker-compose
-DOCKER_COMPOSE_EXEC := $(DOCKER_COMPOSE) exec -T
+DOCKER_COMPOSE_EXEC ?= $(DOCKER_COMPOSE) exec -T
 DOCKER_PROJECT_ROOT ?= /app
 DOCKER_WARNING_INSIDE := You are inside the Docker container!
 
