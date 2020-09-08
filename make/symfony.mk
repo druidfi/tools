139424370c125a14d634b48efdd9d9c5f32b658f
Marko Korhonen <marko.korhonen@druid.fi>
2020-09-08 14:01:38 +0300

    make fresh for symfony

--- make/symfony.mk
+++ make/symfony.mk
@@ -1,5 +1,12 @@
+SF_FRESH_TARGETS := up build sf-about
+
+PHONY += sf-about
 sf-about: ## Displays information about the current project
 	$(call sf_console_on_${RUN_ON},about)
+
+PHONY += fresh
+fresh: ## Build fresh development environment and sync
+	@$(MAKE) $(SF_FRESH_TARGETS)
 
 define sf_console_on_docker
 	$(call docker_run_cmd,bin/console --ansi $(1))
--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=2020-09-08-13-52
+VERSION=2020-09-08-14-01
 DEBUG=0
 GITHUB=https://raw.githubusercontent.com
 REPOSITORY=druidfi/tools
