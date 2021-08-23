57f738a7a4ce089b7296409459d08aa25c8a7716
Marko Korhonen <marko.korhonen@druid.fi>
2021-08-23 10:35:16 +0300

    Make downloader faster to init

--- make/downloader.mk
+++ make/downloader.mk
@@ -1,15 +1,14 @@
 DOWNLOADER_IMAGE := druidfi/backupper-downloader
-DOWNLOADER_IMAGE_EXISTS := $(shell docker inspect --type=image $(DOWNLOADER_IMAGE) >/dev/null 2>&1 && echo "yes" || echo "no")
+DOWNLOADER_URL := https://github.com/druidfi/backupper/releases/tag/latest
+DOWNLOADER_ERROR := Downloader image "$(DOWNLOADER_IMAGE)" was not found!\n\n   Get it from $(DOWNLOADER_URL)\n\n
 
 PHONY += download-dump
 download-dump: ## Download a database dump
 ifeq ($(DUMP_SQL_EXISTS),no)
-ifeq ($(DOWNLOADER_IMAGE_EXISTS),yes)
 	$(call step,Download & extract database dump...)
-	@docker run -it --rm -v $(PWD)/tmp:/app/downloads $(DOWNLOADER_IMAGE) download $(DOWNLOADER_PROJECT)
-	@gunzip tmp/$(DUMP_SQL_FILENAME).gz && mv tmp/$(DUMP_SQL_FILENAME) $(DUMP_SQL_FILENAME) && rm -rf tmp
-else
-	$(call warn,Downloader image "$(DOWNLOADER_IMAGE)" was not found!\n\n   Get it from https://github.com/druidfi/backupper/releases/tag/latest)
-	@exit 1
-endif
+	@docker run -it --rm -v $(PWD)/tmp:/app/downloads $(DOWNLOADER_IMAGE) download $(DOWNLOADER_PROJECT) \
+		&& gunzip tmp/$(DUMP_SQL_FILENAME).gz \
+		&& mv tmp/$(DUMP_SQL_FILENAME) $(DUMP_SQL_FILENAME) \
+		&& rm -rf tmp \
+		|| (printf "\n⚠️  $(DOWNLOADER_ERROR)" && exit 1)
 endif
