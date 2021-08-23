48da25960416c5781a6c4fe1c36ffa9f821730d5
Marko Korhonen <marko.korhonen@druid.fi>
2021-08-23 10:54:31 +0300

    Fix tests failing in GHA linux even if they pass on mac

--- make/downloader.mk
+++ make/downloader.mk
@@ -9,8 +9,8 @@ download-dump: ## Download a database dump
 ifeq ($(DUMP_SQL_EXISTS),no)
 	$(call step,Download & extract database dump...)
 	@docker run -it --rm -v $(PWD)/tmp:/app/downloads $(DOWNLOADER_IMAGE) download $(DOWNLOADER_PROJECT) \
-		&& gunzip tmp/$(DUMP_SQL_FILENAME).gz \
-		&& mv tmp/$(DUMP_SQL_FILENAME) $(DUMP_SQL_FILENAME) \
-		&& rm -rf tmp \
-		|| (printf "\n⚠️  $(DOWNLOADER_ERROR)" && exit 1)
+	&& gunzip tmp/$(DUMP_SQL_FILENAME).gz \
+	&& mv tmp/$(DUMP_SQL_FILENAME) $(DUMP_SQL_FILENAME) \
+	&& rm -rf tmp \
+	|| (printf "\n⚠️  $(DOWNLOADER_ERROR)" && exit 1)
 endif
