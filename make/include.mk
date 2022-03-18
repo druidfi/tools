cea80d7dba0f85ad4fd3b242d7e71bea4ab79197
Marko Korhonen <marko.korhonen@druid.fi>
2022-03-18 15:17:33 +0200

    Remove symfony, wordpress and druid-cli references

--- make/include.mk
+++ make/include.mk
@@ -7,14 +7,6 @@ ifeq ($(IS_DRUPAL),yes)
 include $(DRUIDFI_TOOLS_MAKE_DIR)drupal.mk
 endif
 
-ifeq ($(IS_SYMFONY),yes)
-include $(DRUIDFI_TOOLS_MAKE_DIR)symfony.mk
-endif
-
-ifeq ($(IS_WP),yes)
-include $(DRUIDFI_TOOLS_MAKE_DIR)wordpress.mk
-endif
-
 ifeq ($(SYSTEM),LAGOON)
 include $(DRUIDFI_TOOLS_MAKE_DIR)lagoon.mk
 endif
@@ -29,10 +21,4 @@ PACKAGE_JSON_EXISTS := $(shell test -f $(PACKAGE_JSON_PATH)/package.json && echo
 
 ifeq ($(PACKAGE_JSON_EXISTS),yes)
 include $(DRUIDFI_TOOLS_MAKE_DIR)javascript.mk
-endif
-
-DRUID_CLI_BIN := $(shell command -v druid-cli || echo no)
-
-ifneq ($(DRUID_CLI_BIN),no)
-include $(DRUIDFI_TOOLS_MAKE_DIR)druid-cli.mk
 endif
