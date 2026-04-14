726e4370f77888f6fa42a40aa2523b32c5a14563
Marko Korhonen <marko.korhonen@druid.fi>
2026-04-14 09:21:16 +0300

    Add pnpm support

--- make/javascript.mk
+++ make/javascript.mk
@@ -2,6 +2,7 @@ BUILD_TARGETS += js-install
 JS_PACKAGE_MANAGER ?= yarn
 JS_PACKAGE_MANAGER_CWD_FLAG_NPM ?= --prefix
 JS_PACKAGE_MANAGER_CWD_FLAG_YARN ?= --cwd
+JS_PACKAGE_MANAGER_CWD_FLAG_PNPM ?= --dir
 INSTALLED_NODE_VERSION := $(shell command -v node > /dev/null && node --version | cut -c2-3 || echo no)
 NVM_SH := $(HOME)/.nvm/nvm.sh
 NVM := $(shell test -f "$(NVM_SH)" && echo yes || echo no)
@@ -9,6 +10,7 @@ VOLTA_BIN := $(shell command -v volta || echo no)
 NODE_BIN := $(shell command -v node || echo no)
 NPM_BIN := $(shell command -v npm || echo no)
 YARN_BIN := $(shell command -v yarn || echo no)
+PNPM_BIN := $(shell command -v pnpm || echo no)
 NODE_VERSION ?= 16
 
 # Auto-detect node manager: prefer Volta over NVM. Override with NODE_MANAGER=volta|nvm
@@ -22,10 +24,10 @@ endif
 
 PHONY += js-install
 js-install: ## Install JS packages
-ifeq ($(JS_PACKAGE_MANAGER),yarn)
-	$(call node_run,install --frozen-lockfile)
-else
+ifeq ($(JS_PACKAGE_MANAGER),npm)
 	$(call node_run,install --no-audit --no-fund --engine-strict true)
+else
+	$(call node_run,install --frozen-lockfile)
 endif
 
 PHONY += js-outdated
@@ -36,13 +38,13 @@ js-outdated: ## Show outdated JS packages
 ifeq ($(NODE_MANAGER),volta)
 define node_run
 	$(call step,Run '$(JS_PACKAGE_MANAGER) $(1)' with Volta...\n)
-	@$(JS_PACKAGE_MANAGER) $(if $(filter $(JS_PACKAGE_MANAGER),yarn),$(JS_PACKAGE_MANAGER_CWD_FLAG_YARN),$(JS_PACKAGE_MANAGER_CWD_FLAG_NPM)) $(PACKAGE_JSON_PATH) $(1)
+	@(cd $(PACKAGE_JSON_PATH) && $(JS_PACKAGE_MANAGER) $(1))
 endef
 else ifeq ($(NODE_MANAGER),nvm)
 define node_run
 	$(call step,Run '$(JS_PACKAGE_MANAGER) $(1)' with Node $(NODE_VERSION)...\n)
 	@. $(NVM_SH) && (nvm which $(NODE_VERSION) > /dev/null 2>&1 || nvm install $(NODE_VERSION)) && \
-		nvm exec $(NODE_VERSION) $(JS_PACKAGE_MANAGER) $(if $(filter $(JS_PACKAGE_MANAGER),yarn),$(JS_PACKAGE_MANAGER_CWD_FLAG_YARN),$(JS_PACKAGE_MANAGER_CWD_FLAG_NPM)) $(PACKAGE_JSON_PATH) $(1)
+		nvm exec $(NODE_VERSION) $(JS_PACKAGE_MANAGER) $(if $(filter $(JS_PACKAGE_MANAGER),yarn),$(JS_PACKAGE_MANAGER_CWD_FLAG_YARN),$(if $(filter $(JS_PACKAGE_MANAGER),pnpm),$(JS_PACKAGE_MANAGER_CWD_FLAG_PNPM),$(JS_PACKAGE_MANAGER_CWD_FLAG_NPM))) $(PACKAGE_JSON_PATH) $(1)
 endef
 else
 define node_run
